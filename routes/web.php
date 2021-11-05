48919166b1d53527affc896d1bf6d4579e973fc1
linleckkyalsin <linleckkyalsin@gmail.com>
2021-11-05 17:13:55 +0630

    laravel view lesson 14

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/shop', function () {
+    return view('shops.shop');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/product', function () {
+    return view('product');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
