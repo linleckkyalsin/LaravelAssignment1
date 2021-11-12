95aa10862fc2c1d69c8243e91450783450e24349
linleckkyalsin <linleckkyalsin@gmail.com>
2021-11-12 21:38:46 +0630

    modify template 2

--- resources/views/website/layout/master.blade.php
+++ resources/views/website/layout/master.blade.php
@@ -10,7 +10,7 @@
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
 
 
-    @yield('title')
+   <title> @yield('title')</title>
 
     <!-- Bootstrap core CSS -->
     <link href="{{url('website/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
